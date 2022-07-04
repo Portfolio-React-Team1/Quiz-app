9e88d65dd1c6941ce58d7659acf546b11d1ffd73
Elameen123 <69402639+Elameen123@users.noreply.github.com>
2022-07-04 09:50:13 +0100

    React Quiz App Created

---
+++ quiz-app/src/pages/Alldone/Alldone.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Alldone = () => {
+  return (
+    <div>Alldone</div>
+  )
+}
+
+export default Alldone
---
+++ quiz-app/src/pages/Highscores/Highscores.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Highscores = () => {
+  return (
+    <div>Highscores</div>
+  )
+}
+
+export default Highscores
