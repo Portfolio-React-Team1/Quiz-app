b945dd418a4ca2c153ddb2490feb0485ea777f7b
stanleyChinonso <stancharlesdon@gmail.com>
2022-07-05 10:18:38 +0100

    first commit

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home/Home';
+import Home from './pages/Home/Home'
+import Alldone from './pages/Alldone/Alldone'
 
 function App() {
   return (
     <div className="App">
      <Home/>
+      <Alldone />
     </div>
   );
 }
--- quiz-app/src/pages/Alldone/Alldone.js
+++ quiz-app/src/pages/Alldone/Alldone.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './alldone.css'
 
 const Alldone = () => {
   return (
-    <div>Alldone</div>
+    <div className='alldone'>
+      <div className='alldone-details'>
+        <div className='alldone-h1'><h1>All done!</h1></div>
+        <div alldone-h><h4>Your final score is <span>9.</span></h4></div>
+        <div className='alldone-form'>
+          <form>
+            <label for name='text' >Enter initials:</label>
+            <input name='text' type='text' required></input>
+            <input type='submit' name='submit'></input>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
